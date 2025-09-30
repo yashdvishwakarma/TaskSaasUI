646a31224f3b5a4cc22210bc755da99db8dbcf12
yashdvishwakarma <yashvonmail@gmail.com>
2025-09-30 22:38:10 +0530

    update the packages as vercel still not working with dayjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
-import path from 'path';
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
-
- export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      alias: {
-        'dayjs': path.resolve(__dirname, 'node_modules/dayjs'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      dayjs: path.resolve(__dirname, "node_modules/dayjs"),
     },
-  });
+  },
+});
